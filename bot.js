f6f2e572c048dfd3ad8198ec7e286ff2eb2e4cf0
DingChiLin <gn01168178@yahoo.com.tw>
2018-05-13 16:12:34 +0800

    add onText method

--- bot.js
+++ bot.js
@@ -13,6 +13,18 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
 bot.on('message', (msg) => {
   const name = msg.from.first_name;
   bot.sendMessage(msg.chat.id, 'Tokage: Hello, ' + name + '!').then(() => {

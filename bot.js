1b2900f2a17b1067706a734244f699a35ccc1329
DingChiLin <gn01168178@yahoo.com.tw>
2018-05-28 22:39:53 +0800

    add sleep

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ const stickers = require('./stickers.js')
 const Bot = require('node-telegram-bot-api');
 let bot;
 
+console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
+
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
@@ -11,8 +13,6 @@ if(process.env.NODE_ENV === 'production') {
 else {
   bot = new Bot(token, { polling: true });
 }
-
-console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.onText(/(.+)/, (msg, match) => {
   const chatId = msg.chat.id;
@@ -25,15 +25,26 @@ bot.onText(/(.+)/, (msg, match) => {
   if (resp in stickers){
     const sticker = stickers[resp]
     if (_.isArray(sticker)) {
-      sticker.forEach( s => {
-        bot.sendSticker(chatId, s);
-      })
+      const type = sticker[0]
+      const stickerContent = sticker.slice(1)
+      if (type == 's') {
+        stickerContent.forEach( s => {
+          bot.sendSticker(chatId, s);
+        })
+      } else if (type == 'r') {
+        const index = getRandomInt(stickerContent.length)
+        bot.sendSticker(chatId, stickerContent[index])
+      }
     } else {
       bot.sendSticker(chatId, sticker);
     }
   }
 
 });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
 
 /*
 bot.on('message', (msg) => {

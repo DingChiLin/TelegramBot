f7a39de751f8f0b0c735344fdaad9ffecceff0d1
DingChiLin <gn01168178@yahoo.com.tw>
2019-02-11 12:06:03 +0800

    deploy secret message for timezone testing

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
-const token = process.env.TOKEN;
 const _ = require('lodash');
-const stickers = require('./stickers.js')
 const Bot = require('node-telegram-bot-api');
+const moment = require('moment-timezone');
+const stickers = require('./stickers.js')
+const token = process.env.TOKEN;
+
 let bot;
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
@@ -18,64 +20,113 @@ bot.onText(/(.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
 
+  if (resp == '/test') {
+    bot.sendMessage(chatId, '晚安');
+  }
+
+  if (resp == '/misschi') {
+    m = moment().tz('Asia/Taipei');
+    date = m.format('YYYY-MM-DD');
+    hour = m.format('HH');
+    const message = secretMessage(date, hour)
+    bot.sendMessage(chatId, message);
+
+    const stickers = stickerMessage(message);
+    stickers.forEach( sticker => {
+      if (sticker != '') {
+        bot.sendSticker(chatId, sticker);
+      }
+    })
+
+  }
+
   if (resp == '/help') {
-    bot.sendMessage(chatId, "呆呆：貼圖表!\n\n" + _.keys(stickers).join("\n")  )
+    bot.sendMessage(chatId, "呆呆：貼圖表!\n\n" + _.keys(stickers).join("\n"));
   }
 
-  if (resp.includes('前任') || resp.includes('前女友') ) {
-    bot.sendMessage(chatId, "幹你娘閉嘴")
+  if (resp.includes('前任') || resp.includes('前女友')) {
+    bot.sendMessage(chatId, '幹你娘閉嘴');
   }
 
+  const stickers = stickerMessage(resp);
+  stickers.forEach( sticker => {
+    if (sticker != '') {
+      bot.sendSticker(chatId, sticker);
+    }
+  })
+
+});
+
+function stickerMessage(resp) {
+  var stickerMessages = [];
   _.forEach(stickers, function(sticker, key) {
     if(resp.includes(key)){
       if (_.isArray(sticker)) {
-        const type = sticker[0]
-        const stickerContent = sticker.slice(1)
+        const type = sticker[0];
+        const stickerContent = sticker.slice(1);
         if (type == 's') {
           stickerContent.forEach( s => {
-            bot.sendSticker(chatId, s);
+            stickerMessages.push(s)
           })
         } else if (type == 'r') {
-          const index = getRandomInt(stickerContent.length)
-          bot.sendSticker(chatId, stickerContent[index])
+          const index = getRandomInt(stickerContent.length);
+          stickerMessages = [stickerContent[index]]
         }
       } else {
-        bot.sendSticker(chatId, sticker);
+        stickerMessages = [sticker];
       }
     }
   })
-/*
-  if (resp in stickers){
-    const sticker = stickers[resp]
-    if (_.isArray(sticker)) {
-      const type = sticker[0]
-      const stickerContent = sticker.slice(1)
-      if (type == 's') {
-        stickerContent.forEach( s => {
-          bot.sendSticker(chatId, s);
-        })
-      } else if (type == 'r') {
-        const index = getRandomInt(stickerContent.length)
-        bot.sendSticker(chatId, stickerContent[index])
-      }
-    } else {
-      bot.sendSticker(chatId, sticker);
-    }
-  }
-*/
-});
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max))
+  return stickerMessages
 }
 
-/*
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+function secretMessage(date, hour) {
+  console.log(date);
+  console.log(hour);
 
-  // send a message to the chat acknowledging receipt of their message
-  // bot.sendMessage(chatId, 'Received your message');
-  bot.sendSticker(chatId, 'CAADBQADUgADJZO8Fktt4y20_XaQAg');
-});
-*/
+  var messages = [''];
+  if (date == '2019-02-11' && hour == 12) {
+    return 'ya it is work';
+  } else {
+    return '';
+  }
+
+  if (hour >= 23 || hour <= 8) {
+    messages = ['zzzz', 'zzzz', 'zzzz', 'zzzz', 'zzzz', 'zzzz', 'zzzz', 'zzzz', 'zzzz', 'zzzz', 'zzzz',
+      '被發現了！']
+  } else if (hour >= 8 && hour <= 9) {
+    messages = ['寶貝早安', '']
+  } else if (hour >= 22) {
+    messages = ['寶貝晚安～', '早點睡覺！', '啾啾'];
+  } else {
+    switch(date) {
+      case '2019-02-11':
+        messages = ['內觀第一天，希望寶貝過得很好', '趁機騷你癢耶'];
+        break;
+
+      case '2019-02-22':
+        messages = ['OH!!!'];
+        break;
+
+      case '2019-02-11':
+        messages = ['今天上班辛苦了QQ', '寶貝加油', '呆呆愛媽咪', '走來走去', '摸摸頭'];
+        break;
+
+      default:
+        messages = '想你～';
+    }
+  }
+
+  const index = getRandomInt(messages.length);
+  const secretMessage = messages[index]
+  console.log(index)
+  console.log(messages[index])
+
+  return secretMessage;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 module.exports = bot;
